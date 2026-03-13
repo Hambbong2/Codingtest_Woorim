9fc2671340cc7b8c96f7df5af866e2a812cdffcc
Woorim <yiurimone@gmail.com>
2026-03-13 13:58:33 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,79 +1,36 @@
-from itertools import permutations
-
 N = int(input())
-A_list = list(map(int, input().split())) # [1,2,3,4,5,6]
-tool_num_list = list(map(int, input().split())) # ['+', '+', '-', '*', '/'] 
+num_list = list(map(int,input().split()))
+plus, mius, multiply, divide = map(int, input().split())
+res_list = []
 
-# 데이터 거꾸로 뒤집기 - 스택만들기
-per_st_1 = []
-for i in range(len(A_list)):
-    per_st_1.append(A_list[-i-1])
+def dfs(cur_idx, res, plus, mius, multiply, divide):
     
-# 연산자 리스트 만들기
-# tool_list = ['+','-','*','/']
-
-tool_list = []
-for idx in range(4):
+    # 종료조건 
+    if cur_idx >= N:
+        res_list.append(res)
+        return
     
-    if idx == 0:
-        for _ in range(tool_num_list[0]):
-            tool_list.append('+')
-  
+    if plus > 0:
+        # res = res + num_list[cur_idx]
+        dfs(cur_idx+1, res + num_list[cur_idx] , plus-1, mius, multiply, divide)
     
-    elif idx == 1:
-        for _ in range(tool_num_list[1]):
-            tool_list.append('-')      
-
-
-    elif idx == 2:
-        for _ in range(tool_num_list[2]):
-            tool_list.append('*')
+    if mius > 0:
+        # res = res - num_list[cur_idx]
+        dfs(cur_idx+1, res - num_list[cur_idx] , plus, mius-1, multiply, divide)
     
-    elif idx == 3:
-        for _ in range(tool_num_list[3]):
-            tool_list.append('/')
-
-      
-permutation = set(permutations(tool_list)) # set으로 중복제거
-result = []
-
-for per in permutation:
-    # per_st 다시 초기화..어카지
-    per_st = per_st_1[:]
+    if multiply > 0:
+        # res = res * num_list[cur_idx]
+        dfs(cur_idx+1, res * num_list[cur_idx] , plus, mius, multiply-1, divide)
     
-    for cal in per:
+    if divide > 0: 
         
-        if per_st:
+        dfs(cur_idx+1, int(res / num_list[cur_idx])
+            , plus, mius, multiply, divide-1)
             
-            if cal == '+':
-                c1 = per_st.pop()
-                c2 = per_st.pop()
-                per_st.append(c1 + c2)
-            elif cal == '-':
-                c1 = per_st.pop()
-                c2 = per_st.pop()
-                per_st.append(c1 - c2)
-            elif cal == '*':
-                c1 = per_st.pop()
-                c2 = per_st.pop()
-                per_st.append(c1 * c2)
-            elif cal == '/':
-                c1 = per_st.pop()
-                c2 = per_st.pop()
-                
-                if c1 < 0:
-                    res = -c1 / c2
-                    per_st.append(int(-res))
-                    continue
-                elif c1 == 0:
-                    per_st.append(0)
-                    continue
-                
-                per_st.append(int(c1 / c2))
-    
-    per_res = per_st.pop()
-    result.append(per_res)
+        
 
 
-print(max(result))
-print(min(result))
+dfs(1, num_list[0], plus, mius, multiply, divide)
+
+print(max(res_list))
+print(min(res_list))
